f04bcdbcdd29b14d45bfe5293ec9d1c98389fd1e
theHollowCode <microlchen0616@gmail.com>
2023-04-20 18:50:35 -0400

    Refactored to rank by rankType

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,8 @@ const path = require("path");
 const cors = require('cors');
 
 const app = express();
+
+
 const folderPath = '../data';
 
 async function iterateFolderFiles(folderPath: string) {
@@ -29,16 +31,46 @@ app.use(express.json());
 app.post('/search', (req: any, res: any) => {
   console.log(req.body);
   const query = req.body.query;
-  const rankType = req.body.rankType; // pageRank, HITS, simRank
+  const rankType = req.body.rankType; // pageRank, hits, simRank
+
+  //Just in case papa goose is sending me some weird values.
+  const validRankTypes = ['pageRank', 'hits', 'simRank'];
+  if (!validRankTypes.includes(rankType)) {
+    return res.status(400).send('Invalid rankType');
+  }
 
   const topTenSearches: any[] = [];
   for (const content of contents) {
     const count = [...content.toLowerCase().matchAll(query.toLowerCase())].length;
+    const lines = content.split('\n');
+    //adjust accordingly
+    const pageRank = parseFloat(lines[1]);
+    const simRank = parseFloat(lines[2]);
+    const hits = parseFloat(lines[3]);
+
+    if (!isNaN(pageRank) && !isNaN(simRank) && !isNaN(hits)) {
+      console.log("All Good");
+    } else {
+      if (isNaN(pageRank)) console.log("pageRank Died");
+      if (isNaN(simRank)) console.log("simRank Died");
+      if (isNaN(hits)) console.log("hits Died");
+      console.log("Something is wrong");
+    }
+
     if (count > 0) {
-      topTenSearches.push({ query, content, count });
+      topTenSearches.push({ query, content, count, pageRank, simRank, hits });
     }
   }
-  topTenSearches.sort((a, b) => b.count - a.count);
+  topTenSearches.sort((a, b) => b.count - a.count); //In case there is a tie for pageRank, hits, etc: maybe remove because not worth increase time complex
+  if (rankType === 'pageRank') {
+    topTenSearches.sort((a, b) => b.pageRank - a.pageRank);
+  } else if (rankType === 'hits') {
+    topTenSearches.sort((a, b) => b.hits - a.hits);
+  } else if (rankType === 'simRank') {
+    topTenSearches.sort((a, b) => b.simRank - a.simRank);
+  } else {
+    return res.status(400).send('Invalid rankType');
+  }
   topTenSearches.splice(10);
   res.send(JSON.stringify({
     results: topTenSearches
